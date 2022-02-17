cb2d36b510fbdc0f5c66ca3ebf0a751d86e513ab
sjjung-june <78462290+sjjung-june@users.noreply.github.com>
2022-02-17 09:09:42 +0900

    Vanilla-Assignment5 Done

---
+++ src/index.js
@@ -0,0 +1,23 @@
+const btn = document.querySelector("button");
+const colors = [
+  "#ef5777",
+  "#575fcf",
+  "#4bcffa",
+  "#34e7e4",
+  "#0be881",
+  "#f53b57",
+  "#3c40c6",
+  "#0fbcf9",
+  "#00d8d6",
+];
+
+function btnclickHandler() {
+  const color1 = Math.floor(Math.random() * colors.length);
+  var color2 = Math.floor(Math.random() * colors.length);
+  while (color2 == color1) {
+    color2 = Math.floor(Math.random() * colors.length);
+  }
+  document.body.style.backgroundImage = `linear-gradient(to right, ${colors[color1]}, ${colors[color2]})`;
+}
+
+btn.addEventListener("click", btnclickHandler);
